bfa12cff399e51247bbb6818d10cb6b8407b64d7
juan <juan@jmsg.familia.net>
2012-10-21 19:11:36 -0300

    fix

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ActiveRecord::Base
-    def get_all_ratings
+    def self.get_all_ratings
         movies = self.group("rating")
         return movies.collect { |movie| movie.rating }
     end
